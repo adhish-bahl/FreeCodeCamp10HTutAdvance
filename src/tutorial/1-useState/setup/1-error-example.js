9cabbdccedbc996fffb41417d0e4cdd8b1388712
adhish-bahl <adhishbahl0@gmail.com>
2021-07-11 23:32:43 +0530

    basic structure ready

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  return(
+      <>
+        <h2 style={{marginTop:"60px"}}>useState error example</h2>
+        <h2>Random Title</h2>
+        <button className="btn">Change Title</button>
+      </>
+  );
 };
 
 export default ErrorExample;
