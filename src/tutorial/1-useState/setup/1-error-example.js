e3c84cc8b1a6a2c68adeca3a4d3444ab39fb2297
adhish-bahl <adhishbahl0@gmail.com>
2021-07-11 23:36:42 +0530

    Trying to change the title without use State

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const ErrorExample = () => {
+
+    let title = "Random Title";
+
+    const changeTitle = () => {
+        title = "Hello Friend!";
+    }
+
   return(
       <>
         <h2 style={{marginTop:"60px"}}>useState error example</h2>
-        <h2>Random Title</h2>
-        <button className="btn">Change Title</button>
+        <h2>{title}</h2>
+        <button className="btn" onClick={changeTitle}>Change Title</button>
       </>
   );
 };
