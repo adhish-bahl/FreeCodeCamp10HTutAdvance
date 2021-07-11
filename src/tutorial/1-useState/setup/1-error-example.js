c5285e8fdb15f4865057332727f7c9e07a54738d
adhish-bahl <adhishbahl0@gmail.com>
2021-07-11 23:40:12 +0530

    Chalnging  the title with useState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -3,15 +3,18 @@ import React from 'react';
 const ErrorExample = () => {
 
     let title = "Random Title";
+    const [titleText, setTitleText] = React.useState(title);
 
     const changeTitle = () => {
-        title = "Hello Friend!";
+        // title = "Hello Friend!";
+        setTitleText("Helloe Friends!");
     }
 
   return(
       <>
         <h2 style={{marginTop:"60px"}}>useState error example</h2>
-        <h2>{title}</h2>
+        {/* <h2>{title}</h2> */}
+        <h2>{titleText}</h2>
         <button className="btn" onClick={changeTitle}>Change Title</button>
       </>
   );
