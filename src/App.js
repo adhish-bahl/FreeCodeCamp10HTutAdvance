74c2d0ba92d77668d1af58009492d876f206c065
adhish-bahl <adhishbahl0@gmail.com>
2021-07-12 01:39:51 +0530

    Designing done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import Final from './tutorial/1-useState/final/1-error-example'
-import Setup from './tutorial/1-useState/setup/1-error-example'
+import Final from './tutorial/1-useState/final/3-useState-array'
+import Setup from './tutorial/1-useState/setup/3-useState-array'
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
       <Final/>
+      <hr style={{marginTop:"20px"}} />
       <Setup/>
     </div>
   )
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+
+  const [details, setDeatils] = useState(data);
+
+  return (
+    <>
+      <h2 style={{marginTop:"60px"}}>useState array example</h2>
+      {details.map((detail)=> {
+        const {id, name} = detail;
+        return (
+          <>
+            <div key={id} className='item'>
+              <h4>{name}</h4>
+              <button>remove</button>
+            </div>
+          </>
+        );
+      })}
+      <button className="btn">Clear All</button>
+    </>
+  );
 };
 
 export default UseStateArray;
