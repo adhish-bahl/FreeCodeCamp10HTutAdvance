03c6ddb18ee157c581050db195341a48e042b141
adhish-bahl <adhishbahl0@gmail.com>
2021-07-12 21:04:40 +0530

    all attributes in one object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -4,13 +4,19 @@ const UseStateObject = () => {
 
   const [details, setDetails] = useState({ name: "Peter", age: 25, message: "Random Message"})
 
+  const changeMessage = () => {
+    // setDetails("Hello Guys!");    // Do not work
+    // setDetails({message: "Hello Guys!"});    // Replaces message and delete name and age
+    setDetails({... details, message: "Hello Friend"});    // spread operator    // Works perfectly
+  }
+
   return (
     <>
       <h2>useState object example</h2>
       <h3>{details.name}</h3>
       <h3>{details.age}</h3>
       <h4>{details.message}</h4>
-      <button className="btn">Change Message</button>
+      <button className="btn" onClick={changeMessage}>Change Message</button>
     </>
   );
 };
