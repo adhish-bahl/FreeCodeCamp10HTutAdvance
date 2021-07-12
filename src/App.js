a591562ed8dc3e168dc38b85a03d79516c6c50e7
adhish-bahl <adhishbahl0@gmail.com>
2021-07-12 16:11:44 +0530

    UI done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Final from './tutorial/1-useState/final/3-useState-array'
-import Setup from './tutorial/1-useState/setup/3-useState-array'
+import Final from './tutorial/1-useState/final/4-useState-object'
+import Setup from './tutorial/1-useState/setup/4-useState-object'
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
       <Final/>
-      <hr style={{marginTop:"20px"}} />
+      <hr style={{margin:"20px 0px"}} />
       <Setup/>
     </div>
   )
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+  const [details, setDetails] = useState({ name: "Peter", age: 25, message: "Random Message"})
+
+  return (
+    <>
+      <h2>useState object example</h2>
+      <h3>{details.name}</h3>
+      <h3>{details.age}</h3>
+      <h4>{details.message}</h4>
+      <button className="btn">Change Message</button>
+    </>
+  );
 };
 
 export default UseStateObject;
