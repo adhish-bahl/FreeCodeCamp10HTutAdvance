275bbab9609a46175c24a886a6eadc61a84f7226
adhish-bahl <adhishbahl0@gmail.com>
2021-07-13 01:12:39 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Final from './tutorial/2-useEffect/final/2-useEffect-cleanup'
-import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+import Final from './tutorial/2-useEffect/final/3-useEffect-fetch-data'
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
       <hr style={{margin:"20px 0px"}} />
-      {/* <Final/> */}
+      <Final/>
       <hr style={{margin:"20px 0px"}} />
       <Setup/>
     </div>
