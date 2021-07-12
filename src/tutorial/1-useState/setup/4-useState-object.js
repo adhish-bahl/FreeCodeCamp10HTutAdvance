db243a04bf7a52a0869ac4f30898264bcd9aca52
adhish-bahl <adhishbahl0@gmail.com>
2021-07-12 21:05:04 +0530

    all attributes in different object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -4,11 +4,19 @@ const UseStateObject = () => {
 
   const [details, setDetails] = useState({ name: "Peter", age: 25, message: "Random Message"})
 
+  // const [name, setName] = useState("Nitin");
+  // const [age, setAge] = useState(23);
+  // const [message, setMessage] = useState("Random Message");
+
   const changeMessage = () => {
     // setDetails("Hello Guys!");    // Do not work
     // setDetails({message: "Hello Guys!"});    // Replaces message and delete name and age
     setDetails({... details, message: "Hello Friend"});    // spread operator    // Works perfectly
   }
+
+  // const changeMessageSingle = () => {
+  //   setMessage("Hello Guys");
+  // }
 
   return (
     <>
@@ -16,6 +24,8 @@ const UseStateObject = () => {
       <h3>{details.name}</h3>
       <h3>{details.age}</h3>
       <h4>{details.message}</h4>
+      {/* <h4>{message}</h4> */}
+      {/* <button className="btn" onClick={changeMessageSingle}>Change Message</button> */}
       <button className="btn" onClick={changeMessage}>Change Message</button>
     </>
   );
