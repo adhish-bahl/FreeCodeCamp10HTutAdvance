eb404dee5e6c5fd82554cbc513f2e13d9256ab5b
adhish-bahl <adhishbahl0@gmail.com>
2021-07-13 00:30:49 +0530

    document title updating done

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,11 +3,21 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(()=> {
+    if(count > 0)
+      document.title = `${count} new messages`;
+  });
+
+
+
   return (
     <>
       <h2>useEffect Basics</h2>
-      <h1>0</h1>
-      <button className="btn">Click Me</button>
+      <h1>{count}</h1>
+      <button className="btn" onClick={() => setCount(count+1)}>Click Me</button>
     </>
     );
 };
