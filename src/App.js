2813d37421388fdb6353892005be650248cbf9e6
adhish-bahl <adhishbahl0@gmail.com>
2021-07-12 22:32:47 +0530

    UI done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Final from './tutorial/1-useState/final/4-useState-object'
-import Setup from './tutorial/1-useState/setup/4-useState-object'
+import Final from './tutorial/1-useState/final/5-useState-counter'
+import Setup from './tutorial/1-useState/setup/5-useState-counter'
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
+      <hr style={{margin:"20px 0px"}} />
       <Final/>
       <hr style={{margin:"20px 0px"}} />
       <Setup/>
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  return (
+    <>
+      <h2>useState counter example</h2>
+      <h2 style={{marginTop:"60px"}}>Regular Counter</h2>
+      <h1>0</h1>
+      <button className="btn">Decrease</button>
+      <button className="btn">Reset</button>
+      <button className="btn">Increase</button>
+      <h2 style={{marginTop:"60px"}}>Regular Counter</h2>
+      <h1>0</h1>
+      <button className="btn" style={{marginBottom:"60px"}}>Increase Later</button>
+    </>
+    );
 };
 
 export default UseStateCounter;
