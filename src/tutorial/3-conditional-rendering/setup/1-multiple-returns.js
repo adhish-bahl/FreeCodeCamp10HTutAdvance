9973a53feab316b2276db30b4d3045de33875727
adhish-bahl <adhishbahl0@gmail.com>
2021-07-13 10:15:24 +0530

    done

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,59 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const getName = () => {
+    fetch(url)
+    .then((response123)=>{
+      if(response123.status >= 200 && response123.status <= 299)
+        return response123.json();
+      else{
+        setLoading(false);
+        setError(true);
+        throw new Error(response123.status);
+      }
+    })
+    .then((details)=>{
+      console.log(details);
+      const user = details.login;
+      setName(user);
+      setLoading(false);
+    })
+    .catch((err)=>{
+      console.log(err);
+      setLoading(false);
+      setError(true);
+    })
+  }
+
+  useEffect(()=>{
+    getName();
+  },[])
+
+
+  if(loading) {
+    return(
+      <h1>Loading...</h1>
+    )
+  }
+
+  if(error) {
+    return(
+      <h1>Error...</h1>
+    )
+  }
+  
+  return (
+    <>
+      <h2>multiple returns</h2>
+      <h1>{name}</h1>
+    </>
+    );
 };
 
 export default MultipleReturns;
