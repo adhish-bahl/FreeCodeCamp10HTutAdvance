092fbbe8418bd547e1cd2de014675d401f959db9
adhish-bahl <adhishbahl0@gmail.com>
2021-07-13 11:49:11 +0530

    done

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -6,7 +6,17 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const [error, setError] = useState(false);
+
+  return (
+    <>
+      <h2>short circuit</h2>
+      <h1>John Doe</h1>
+      <button className="btn" onClick={()=>setError(!error)}>Toggle Error</button>
+      {error && <h2>Error</h2>}
+      {error ? <p>Error Occured</p> : <h1>There is No error</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
