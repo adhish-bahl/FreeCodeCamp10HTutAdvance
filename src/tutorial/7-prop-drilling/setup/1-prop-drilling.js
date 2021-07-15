278b15c0f033dd4ba04c4dfe24e3f48d7dc23f36
adhish-bahl <adhishbahl0@gmail.com>
2021-07-15 22:25:21 +0530

    first component done

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
+import { data } from '../../../data';
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+
+  const [people, setPeople] = useState(data);
+
+  const removePerson = (id) => {
+    const newPeople = people.filter((people) => people.id !== id)
+    setPeople(newPeople);
+  }
+
+  return (
+    <>
+      <h2>prop drilling</h2>
+      <List people={people} removePerson={removePerson} />
+    </>
+  );
 };
+
+const List = () => {
+
+}
 
 export default PropDrilling;
