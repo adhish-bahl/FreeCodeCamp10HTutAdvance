7365fb2a1ca613e1d66bf5e456c1ebb38ea8f21e
adhish-bahl <adhishbahl0@gmail.com>
2021-07-16 17:44:17 +0530

    basic done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Final from './tutorial/9-custom-hooks/final/1-fetch-example'
-import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example'
+import Final from './tutorial/10-prop-types/final/'
+import Setup from './tutorial/10-prop-types/setup/'
 function App() {
   return (
     <div className='container'>
