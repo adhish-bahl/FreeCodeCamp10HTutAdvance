844780e53595eb4ba97d917e76a8da4b38de3ef0
adhish-bahl <adhishbahl0@gmail.com>
2021-07-16 18:06:53 +0530

    declaring fields as required

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
 
 const Product = ({name, image, price}) => {
   return (
@@ -8,6 +10,12 @@ const Product = ({name, image, price}) => {
     <p>${price}</p>
   </article>
   );
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  price: PropTypes.number.isRequired,
 };
 
 export default Product;
