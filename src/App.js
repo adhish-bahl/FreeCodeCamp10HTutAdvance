d19eef39c14664fd476cc230b33725a8787c913f
adhish-bahl <adhishbahl0@gmail.com>
2021-07-17 23:20:24 +0530

    made navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Final from './tutorial/10-prop-types/final/'
-import Setup from './tutorial/10-prop-types/setup/'
+import Final from './tutorial/11-react-router/final/'
+import Setup from './tutorial/11-react-router/setup/'
 function App() {
   return (
     <div className='container'>
--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
-// react router
+
+// importing react router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 // pages
 import Home from './Home';
 import About from './About';
 import People from './People';
 import Error from './Error';
 import Person from './Person';
+
 // navbar
 import Navbar from './Navbar';
+
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return(
+    <Router>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Router>
+  )
 };
 
 export default ReactRouterSetup;
